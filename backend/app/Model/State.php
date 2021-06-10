156429b9d44cba85b178ec743ee386f0dcf3ecfa
vish <vishalmaniya@gmail.com>
2021-06-10 16:06:16 -0400

    Fix: update state query to get unique one

--- backend/app/Model/State.php
+++ backend/app/Model/State.php
@@ -14,6 +14,7 @@ class State extends Model
     public static function getStates($term = null) {
         return static::select('id', 'name')
             ->where("name","LIKE","%{$term}%")
+            ->distinct()
             ->get();
     }
 }
